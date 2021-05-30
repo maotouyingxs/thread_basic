f57edbf4bb081e0664ffee7955549f660242aee0
maotouyingxs <renslc@163.com>
2021-05-30 18:20:07 +0800

    多线程的创建方式

--- src/main/java/com/mao/thread_basic/Thread02.java
+++ src/main/java/com/mao/thread_basic/Thread02.java
@@ -1,19 +1,20 @@
 package com.mao.thread_basic;
 
-public class Thread01 {
+public class Thread02 {
     public static void main(String[] args) {
         System.out.println("主线程开始");
         // 1.创建线程
-        ThreadDemo01 threadDemo01 = new ThreadDemo01();
+        ThreadDemo02 threadDemo02 = new ThreadDemo02();
+        Thread t1 = new Thread(threadDemo02);
         // 2.启动线程
-        threadDemo01.start();
+        t1.start();
         for (int i = 0; i < 10; i++) {
             System.out.println("主...i:" + i);
         }
     }
 }
 
-class ThreadDemo01 extends Thread {
+class ThreadDemo02 implements Runnable {
     @Override
     public void run() {
         for (int i = 0; i < 10; i++) {
