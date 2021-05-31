2e7f1cfceca84b380e73b6872bc8d2f3335a0a76
maotouyingxs <renslc@163.com>
2021-05-31 19:07:56 +0800

    静态代码块

--- src/main/java/com/mao/threadsafe/Test01.java
+++ src/main/java/com/mao/threadsafe/Test01.java
@@ -8,35 +8,79 @@ package com.mao.threadsafe;
  * @Date 2021/5/30 22:32
  */
 public class Test01 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         ThreadDemo01 threadDemo01 = new ThreadDemo01();
         Thread t1 = new Thread(threadDemo01, "窗口1");
         Thread t2 = new Thread(threadDemo01, "窗口2");
         t1.start();
+        Thread.sleep(40);
+        threadDemo01.flag = false;
         t2.start();
     }
 }
 
 class ThreadDemo01 implements Runnable {
 
-    private int count = 100;
+    private static int count = 100;
+    private Object oj = new Object();
+    public boolean flag = true;
 
     @Override
     public void run() {
-        while (count > 0) {
-            try {
-                Thread.sleep(50);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+        if (flag) {
+            while (count > 0) {
+                try {
+                    Thread.sleep(50);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                synchronized (this) {
+                    if (count > 0) {
+                        System.out.println(Thread.currentThread().getName() + "，出售第" + (100 - count + 1) + "张票");
+                        count--;
+                    }
+                }
             }
-            sale();
+        } else {
+            while (count > 0) {
+                try {
+                    Thread.sleep(50);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                sale();
+            }
         }
     }
 
+    /**
+     * 同步方法方式
+     */
     public synchronized void sale() {
         if (count > 0) {
             System.out.println(Thread.currentThread().getName() + "，出售第" + (100 - count + 1) + "张票");
             count--;
         }
     }
+
+    /**
+     * 同步代码块方式
+     */
+    // public void sale() {
+    //     synchronized (this) {
+    //         if (count > 0) {
+    //             System.out.println(Thread.currentThread().getName() + "，出售第" + (100 - count + 1) + "张票");
+    //             count--;
+    //         }
+    //     }
+    // }
+    /**
+     * 静态同步代码块方式（锁是当前类的字节码文件）
+     */
+    // public static synchronized  void sale() {
+    //     if (count > 0) {
+    //         System.out.println(Thread.currentThread().getName() + "，出售第" + (100 - count + 1) + "张票");
+    //         count--;
+    //     }
+    // }
 }
